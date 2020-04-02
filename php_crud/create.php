260fbb1f2209fbdac456f9def69e4e2f3ed85a33
Otmane Douieb <45903432+douiebotmane@users.noreply.github.com>
2020-04-02 17:43:45 +0100

    Add files via upload

---
+++ php_crud/create.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/style_create.css">
+    <title>Add User</title>
+ 
+</head>
+<body>
+
+
+<div class="container">
+  <h2>Add User</h2>
+  <form action="php_action/create.php" method="post">
+    <div class="form-group">
+      <label for="name">Name:</label>
+      <input type="text" class="form-control" id="name" placeholder="Enter name" name="name">
+    </div>
+    <div class="form-group">
+      <label for="email">Email:</label>
+      <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+    </div>
+    <div class="form-group">
+      <label for="name">Login:</label>
+      <input type="text" class="form-control" id="login" placeholder="Enter login" name="login">
+    </div>
+    <div class="form-group">
+      <label for="password">Password:</label>
+      <input type="password" class="form-control" id="password" placeholder="Enter password" name="password">
+    </div>
+    <div class="form-group form-check">
+      <label class="form-check-label">
+        <input class="form-check-input" type="checkbox" name="remember"> Remember me
+      </label>
+    </div>
+    <button type="submit" class="btn btn-success">Submit</button>
+    <a href="index.php"><button type="button" class="btn btn-primary">Back</button></a>
+  </form>
+</div>
+
+ 
+</body>
+</html>
